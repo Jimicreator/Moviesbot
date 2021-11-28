81031ea70df68a8e61ffd553ea13d4a23f953719
Jimicreator <94049086+Jimicreator@users.noreply.github.com>
2021-11-28 09:00:43 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🔰 Our Group 🔰', url=f'{Tutorial}')
+                        InlineKeyboardButton('🔰 Our Group 🔰', url=f'{TUTORIAL}')
                     ]
                     ]
                 await bot.send_cached_media(
@@ -331,7 +331,7 @@ async def bot_info(bot, message):
     buttons = [
         [
             
-            InlineKeyboardButton('Want to Create Own 😎', url=f'{Tutorial}')
+            InlineKeyboardButton('Want to Create Own 😎', url=f'{TUTORIAL}')
         ]
         ]
     await message.reply(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
